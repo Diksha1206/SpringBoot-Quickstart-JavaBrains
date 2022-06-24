a1a9d035520db3d28c917da907fe1bee76893f04
Diksha Satpute <dikshasatpute@boomi.com>
2022-06-24 16:20:57 -0400

    Basic spring boot project. Create a controller and Service. Use basic annotations to handle request and give out a json respone

---
+++ course-api/src/main/java/com/javabrains/courseapi/Topic/TopicService.java
@@ -0,0 +1,41 @@
+package com.javabrains.courseapi.Topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("1", "topic1", "description1"),
+            new Topic("2", "topic2", "description2"),
+            new Topic("3", "topic3", "description3")
+    ));
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i=0; i< topics.size(); i++) {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+        return;
+    }
+}
